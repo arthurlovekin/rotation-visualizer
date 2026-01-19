117f0491241202e313cd9b6a9001147010d0f7b0
Arthur Lovekin <arthur.lovekin@generalrobotics.company>
2026-01-18 20:10:47 -0800

    start to add leptos

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use leptos::mount::mount_to_body;
+use leptos::prelude::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| *set_count.write() += 1
+            class:red=move || count.get() % 2 == 1
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
+        <progress
+            max="50"
+            // signals are functions, so `value=count` and `value=move || count.get()`
+            // are interchangeable.
+            value=count
+        />
+    }
+}
+
 use three_d::*;
 
 pub fn main() {
+    mount_to_body(App); // Leptos
+
     let window = Window::new(WindowSettings {
         title: "Shapes!".to_string(),
         max_size: Some((1280, 720)),
