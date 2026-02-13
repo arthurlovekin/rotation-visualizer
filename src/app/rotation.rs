e7db8103e4dbe9b91ccd5d72074122194251e11c
Arthur Lovekin <arthur.lovekin@generalrobotics.company>
2026-02-12 17:58:50 -0800

    Rotation matrix added

--- src/app/rotation.rs
+++ src/app/rotation.rs
@@ -1,4 +1,5 @@
 use std::ops::Mul;
+use std::ops::{Index, IndexMut};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Quaternion {
@@ -51,6 +52,17 @@ impl Mul for Quaternion {
     }
 }
 
+impl From<RotationMatrix> for Quaternion {
+    fn from(matrix: RotationMatrix) -> Self {
+        let mut quat = Quaternion::default();
+        quat.w = (1.0 + matrix[0][0] + matrix[1][1] + matrix[2][2]) / 2.0;
+        quat.x = (matrix[2][1] - matrix[1][2]) / (4.0 * quat.w);
+        quat.y = (matrix[0][2] - matrix[2][0]) / (4.0 * quat.w);
+        quat.z = (matrix[1][0] - matrix[0][1]) / (4.0 * quat.w);
+        quat
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct AxisAngle {
     pub x: f32,
@@ -81,6 +93,69 @@ impl AxisAngle {
     }
 }
 
+pub struct RotationMatrix(pub [[f32; 3]; 3]);
+
+impl Index<usize> for RotationMatrix {
+    type Output = [f32; 3];
+
+    #[inline]
+    fn index(&self, row: usize) -> &Self::Output {
+        &self.0[row]
+    }
+}
+
+impl IndexMut<usize> for RotationMatrix {
+    #[inline]
+    fn index_mut(&mut self, row: usize) -> &mut Self::Output {
+        &mut self.0[row]
+    }
+}
+
+impl Default for RotationMatrix {
+    fn default() -> Self {
+        Self([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+    }
+}
+
+impl Mul for RotationMatrix {
+    type Output = RotationMatrix;
+
+    fn mul(self, other: RotationMatrix) -> RotationMatrix {
+        let mut result = RotationMatrix([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]);
+        for i in 0..3 {
+            for j in 0..3 {
+                result[i][j] = 0.0;
+                for k in 0..3 {
+                    result[i][j] += self[i][k] * other[k][j];
+                }
+            }
+        }
+        result
+    }
+}
+
+impl From<Quaternion> for RotationMatrix {
+    fn from(quat: Quaternion) -> Self {
+        RotationMatrix([
+            [
+                1.0 - 2.0 * (quat.y * quat.y + quat.z * quat.z),
+                2.0 * (quat.x * quat.y - quat.w * quat.z),
+                2.0 * (quat.x * quat.z + quat.w * quat.y),
+            ],
+            [
+                2.0 * (quat.x * quat.y + quat.w * quat.z),
+                1.0 - 2.0 * (quat.x * quat.x + quat.z * quat.z),
+                2.0 * (quat.y * quat.z - quat.w * quat.x),
+            ],
+            [
+                2.0 * (quat.x * quat.z - quat.w * quat.y),
+                2.0 * (quat.y * quat.z + quat.w * quat.x),
+                1.0 - 2.0 * (quat.x * quat.x + quat.y * quat.y),
+            ],
+        ])
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Rotation {
     quat: Quaternion,
@@ -100,6 +175,11 @@ impl Rotation {
     pub fn as_axis_angle(&self) -> AxisAngle {
         AxisAngle::from_quaternion(self.quat)
     }
+
+    pub fn as_rotation_matrix(&self) -> RotationMatrix {
+        RotationMatrix::from(self.quat)
+    }
+
 }
 
 impl From<Quaternion> for Rotation {
@@ -112,6 +192,14 @@ impl From<AxisAngle> for Rotation {
     fn from(axis_angle: AxisAngle) -> Self {
         Rotation {
             quat: axis_angle.as_quaternion(),
+        }
+    }
+}
+
+impl From<RotationMatrix> for Rotation {
+    fn from(matrix: RotationMatrix) -> Self {
+        Rotation {
+            quat: (matrix).into(),
         }
     }
 }
